44c8b42f20e55c15fc252771d927ed3d0ef70c8f
Stepan Rozhkov <46667510+stepanro@users.noreply.github.com>
2022-12-26 10:17:49 +0000

    test

--- stevenhorn/victorina/views.py
+++ stevenhorn/victorina/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def victorina(requests):
-    return HttpResponse('Это страница викторины!')
+    return HttpResponse('Это страница!')
 
 def admin(requests):
     return HttpResponse('админка')
