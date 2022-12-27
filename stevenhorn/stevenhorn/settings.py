6ce9ffd9c0e46c22454e993b221575801ec329ad
stevenhorn <stepanrojkov@icloud.com>
2022-12-27 20:54:17 +0300

    add new dir in templates dirs

--- stevenhorn/stevenhorn/settings.py
+++ stevenhorn/stevenhorn/settings.py
@@ -59,7 +59,9 @@ ROOT_URLCONF = 'stevenhorn.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'public_html'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
