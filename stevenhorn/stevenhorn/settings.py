fabb442e4cef3f3d1bcd92b747899466b43c4101
stevenhorn <stepanrojkov@icloud.com>
2022-12-27 23:51:49 +0300

    test

--- stevenhorn/stevenhorn/settings.py
+++ stevenhorn/stevenhorn/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'stevenhorn.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,6 +126,17 @@ STATICFILES_DIR = []
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+ITERNAL_IPS = [
+    '127.0.0.1',
+]
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': os.path.join(BASE_DIR, 'coolsite_cache')
+    }
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
