3c88e7bbd386d506d2f06e7282fe8d16dcff0d13
Stepan Rozhkov <46667510+stepanro@users.noreply.github.com>
2022-12-27 11:49:53 +0000

    fix views

--- stevenhorn/victorina/views.py
+++ stevenhorn/victorina/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def button(requests):
-    return render(requests, 'victorina/index.html')
+def button(request):
+    return render(request, 'victorina/index.html')
