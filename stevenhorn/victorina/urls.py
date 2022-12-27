23ae384312c1b9d1ed03b2a252f12867ef81ca11
Stepan Rozhkov <46667510+stepanro@users.noreply.github.com>
2022-12-27 08:17:07 +0000

    add folder

--- stevenhorn/victorina/urls.py
+++ stevenhorn/victorina/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', victorina),
-    path('vs/', admin),
+    path('button/', button),
 ]
--- stevenhorn/victorina/views.py
+++ stevenhorn/victorina/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def victorina(requests):
-    return HttpResponse('Это страница!')
-
-def admin(requests):
-    return HttpResponse('админка')
+def button(requests):
+    return render(requests, '')
